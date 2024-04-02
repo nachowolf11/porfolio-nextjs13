9c53466754488012f447777be4724a6586d0f73d
nachowolf11 <ignaciowolf10@gmail.com>
2024-04-02 13:32:39 -0300

    Favicons

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -10,13 +10,13 @@ interface Props {
   }
 }
 
-export async function generateMetadata({params: {locale}}:Props) {
-  const t = await getTranslations({locale, namespace: 'metadata.index'});
- 
+export async function generateMetadata({ params: { locale } }: Props) {
+  const t = await getTranslations({ locale, namespace: 'metadata.index' });
+
   return {
     title: t('title'),
-    description:t('description'),
-    keywords:t('keywords'),
+    description: t('description'),
+    keywords: t('keywords'),
   };
 }
 
@@ -28,10 +28,29 @@ export default function RootLayout({
   params: { locale: string }
 }>) {
   return (
-    <html lang={ locale } suppressHydrationWarning>
+    <html lang={locale} suppressHydrationWarning>
+      <head>
+        <link rel="apple-touch-icon" sizes="57x57" href="/favicons/apple-icon-57x57.png" />
+        <link rel="apple-touch-icon" sizes="60x60" href="/favicons/apple-icon-60x60.png" />
+        <link rel="apple-touch-icon" sizes="72x72" href="/favicons/apple-icon-72x72.png" />
+        <link rel="apple-touch-icon" sizes="76x76" href="/favicons/apple-icon-76x76.png" />
+        <link rel="apple-touch-icon" sizes="114x114" href="/favicons/apple-icon-114x114.png" />
+        <link rel="apple-touch-icon" sizes="120x120" href="/favicons/apple-icon-120x120.png" />
+        <link rel="apple-touch-icon" sizes="144x144" href="/favicons/apple-icon-144x144.png" />
+        <link rel="apple-touch-icon" sizes="152x152" href="/favicons/apple-icon-152x152.png" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/favicons/apple-icon-180x180.png" />
+        <link rel="icon" type="image/png" sizes="192x192" href="/favicons/android-icon-192x192.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicons/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="96x96" href="/favicons/favicon-96x96.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicons/favicon-16x16.png" />
+        <link rel="manifest" href="/favicons/manifest.json" />
+        <meta name="msapplication-TileColor" content="#ffffff" />
+        <meta name="msapplication-TileImage" content="/favicons/ms-icon-144x144.png" />
+        <meta name="theme-color" content="#ffffff" />
+      </head>
       <body className="bg-slate-900 leading-relaxed text-slate-400 antialiased selection:bg-teal-300 selection:text-teal-900">
-        <GoogleAnalytics/>
-        <Mousefollow/>
+        <GoogleAnalytics />
+        <Mousefollow />
         {children}
       </body>
     </html>
